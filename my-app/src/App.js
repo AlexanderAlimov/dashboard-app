77e68ab5aed71f7bf1c719edf377d4039aa2676a
sasha.alimov <sasha.alimov5@gmail.com>
2019-10-30 13:17:46 +0200

    create app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+function App() {
+  return (
+		<div>
+			<div>Hello Test Component!</div>
+			<DatePicker />
+		</div>
+  );
+}
+
+export default App;
