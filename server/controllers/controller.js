76c40e7873772bc19f8dc70a64ce89309713726b
sasha.alimov <sasha.alimov5@gmail.com>
2019-11-14 12:30:35 +0200

    refactor promises

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,94 +1,114 @@
 import Product from "../models/product.model";
 import Category from "../models/category.model";
 
-const ifError = (err, res) => {
-  if (err) {
-    res.status(500).send({ message: err });
-    return;
-  }
-};
+const handleError = (err, res) => res.status(500).send({ message: err });
 
 class Controller {
   getCategories(req, res, next) {
-    Category.find((err, categories) => {
-      ifError(err, res);
-      res.status(200).send({
-        data: categories
+    Category.find({})
+      .then(categories => {
+        res.status(200).send({
+          data: categories
+        });
+      })
+      .catch(err => {
+        if (err) handleError(err, res);
       });
-    });
   }
 
   getProducts(req, res, next) {
     const id = req.query.category;
     let category = id ? { category: id } : {};
-    Product.find(category, (err, products) => {
-      ifError(err, res);
-      res.status(200).send({
-        data: products
+    Product.find(category)
+      .then(products => {
+        res.status(200).send({
+          data: products
+        });
+      })
+      .catch(err => {
+        if (err) handleError(err, res);
       });
-    });
   }
 
   addProduct(req, res, next) {
-    Category.find({ name: req.body.category }, (err, category) => {
-      ifError(err, res);
-      const id = category[0]._id;
-
-      let product = new Product({
-        name: req.body.name,
-        purchPrice: req.body.purchPrice,
-        salePrice: req.body.salePrice,
-        category: id
-      });
-
-      product.save(err => {
-        ifError(err, res);
-        res.status(200).send({
-          data: product
+    Category.find({ name: req.body.category })
+      .then(data => {
+        const id = data[0]._id;
+        let product = new Product({
+          name: req.body.name,
+          purchPrice: req.body.purchPrice,
+          salePrice: req.body.salePrice,
+          category: id
         });
+        product
+          .save()
+          .then(product => {
+            res.status(200).send({
+              data: product
+            });
+          })
+          .catch(err => {
+            if (err) handleError(err, res);
+          });
+      })
+      .catch(err => {
+        if (err) handleError(err, res);
       });
-    });
   }
 
   addCategory(req, res, next) {
     let category = new Category({ name: req.body.name });
-    category.save(err => {
-      ifError(err, res);
-      res.status(200).send({
-        data: category
+    category
+      .save()
+      .then(category => {
+        res.status(200).send({
+          data: category
+        });
+      })
+      .catch(err => {
+        if (err) handleError(err, res);
       });
-    });
   }
 
   removeProduct(req, res, next) {
     const id = req.params.id;
-    Product.findByIdAndRemove(id, (err, product) => {
-      ifError(err, res);
-      res.status(200).send({
-        data: product._id
+    Product.findByIdAndRemove(id)
+      .then(product => {
+        res.status(200).send({
+          data: product._id
+        });
+      })
+      .catch(err => {
+        if (err) handleError(err, res);
       });
-    });
   }
 
   removeCategory(req, res, next) {
     const id = req.params.id;
     let categoryId;
-    Category.findByIdAndRemove(id, (err, category) => {
-      ifError(err, res);
-      Category.find({ name: "Without Category" }, (err, category) => {
-        ifError(err, res);
-        categoryId = category[0]._id;
-        Product.find({ category: id }, (err, products) => {
-          ifError(err, res);
-          products.forEach(product => {
-            product.category = categoryId;
-            product.save();
+    Category.findByIdAndRemove(id).then(category => {
+      Category.find({ name: "Without Category" })
+        .then(category => {
+          categoryId = category[0]._id;
+          Product.find({ category: id })
+            .then(products => {
+              products.forEach(product => {
+                product.category = categoryId;
+                product.save();
+              });
+            })
+            .catch(err => {
+              if (err) handleError(err, res);
+            });
+        })
+        .then(() => {
+          res.status(200).send({
+            data: category._id
           });
+        })
+        .catch(err => {
+          if (err) handleError(err, res);
         });
-      });
-      res.status(200).send({
-        data: category._id
-      });
     });
   }
 
@@ -98,21 +118,28 @@ class Controller {
       purchPrice = Number(req.body.purchPrice),
       salePrice = Number(req.body.salePrice);
 
-    Category.find({ name: category }, (err, category) => {
-      ifError(err);
-      const categoryId = category[0]._id;
-      Product.find({ _id: req.params.id }, (err, product) => {
-        ifError(err, res);
-        product[0].name = name;
-        product[0].purchPrice = purchPrice;
-        product[0].salePrice = salePrice;
-        product[0].category = categoryId;
-        product[0].save();
-        res.status(200).send({
-          data: product[0]
-        });
+    Category.find({ name: category })
+      .then(category => {
+        const categoryId = category[0]._id;
+        Product.find({ _id: req.params.id })
+          .then(item => {
+            const product = item[0];
+            product.name = name;
+            product.purchPrice = purchPrice;
+            product.salePrice = salePrice;
+            product.category = categoryId;
+            product.save();
+            res.status(200).send({
+              data: product
+            });
+          })
+          .catch(err => {
+            if (err) handleError(err);
+          });
+      })
+      .catch(err => {
+        if (err) handleError(err);
       });
-    });
   }
 }
 
